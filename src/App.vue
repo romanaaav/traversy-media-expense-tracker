c5ffa5e24c73e72fdb76e6a15299f88f68110334
Roman Alicando <romanaaav@gmail.com>
2024-01-31 23:06:09 -0300

    Balance, expenses and income using Vue Compostion API.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <Header/>
   <div class="container">
-    <Balance/>
-    <IncomeExpense/>
-    <TransactionList/>
+    <Balance :total="total"/>
+    <IncomeExpense :income="income" :expenses="expenses"/>
+    <TransactionList :transactions="transactions"/>
     <AddTransaction/>
   </div>
 </template>
 
-<script>
-  import Header from './components/Header.vue'
-  import Balance from './components/Balance.vue'
-  import IncomeExpense from './components/IncomeExpense.vue'
-  import TransactionList from './components/TransactionList.vue'
-  import AddTransaction from './components/AddTransaction.vue'
+<script setup>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import IncomeExpense from './components/IncomeExpense.vue';
+  import TransactionList from './components/TransactionList.vue';
+  import AddTransaction from './components/AddTransaction.vue';
 
-  export default {
-    name: 'App',
-    components: {
-      Header,
-      Balance,
-      IncomeExpense,
-      TransactionList,
-      AddTransaction
-    }
-  }
+  import {ref, computed} from 'vue';
+
+  const transactions = ref([
+    {id: 1, text: 'Flower', amount: -19.99},
+    {id: 2, text: 'Salary', amount: 299.97},
+    {id: 3, text: 'Book', amount: -10},
+    {id: 4, text: 'Camera', amount: 150},
+  ])
+
+  //Get total
+  const total = computed(()=> {
+    return transactions.value.reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0);
+  })
+
+  //Get income
+  const income = computed(()=> {
+    return transactions.value.filter(transaction => transaction.amount > 0).reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0).toFixed(2);
+  })
+
+  //Get expenses
+  const expenses = computed(()=> {
+    return transactions.value.filter(transaction => transaction.amount < 0).reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0).toFixed(2);
+  })
+
+
 </script>
